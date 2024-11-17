799413cb22413e1802a643fbe3a7259c9aec9e76
Wahyu Agus Arifin <itpohgero@gmail.com>
2024-11-17 16:32:28 +0700

    setup micro frontend

---
+++ apps/docs/src/app/docs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+	return (
+		<div>APP DOCS</div>
+	)
+}
+
+export default Page
--- apps/main/next.config.ts
+++ apps/main/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const DOCS_URL = 'http://localhost:8001';  // docs app is running here
 const nextConfig: NextConfig = {
-	/* config options here */
+	assetPrefix: '/docs-static',
+	async rewrites() {
+		return [
+			{
+				source: '/docs',
+				destination: `${DOCS_URL}/docs`,  // This should correctly route the /docs path
+			},
+			{
+				source: '/docs/:path*',
+				destination: `${DOCS_URL}/docs/:path*`,  // Ensure the path is passed correctly
+			},
+			{
+				source: '/docs-static/:path*',
+				destination: `${DOCS_URL}/docs-static/:path*`,  // Static files must be accessible from this path
+			},
+		]
+	},
 };
 
 export default nextConfig;
